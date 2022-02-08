0e85e556cec0048ab5952235e838fdf103eda3d9
rogeriogomes2009 <rogerio.gomes2009@gmail.com>
2022-02-08 02:05:53 -0300

    New Update

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 const mongo = process.env.MONGO || 'mongodb://localhost/minhas-series-rest'
+
+const bodyParser = require('body-parser')
+app.use(bodyParser({extended: true}))
+
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
--- routes/series.js
+++ routes/series.js
@@ -7,7 +7,26 @@ router.get('/', async (req, res) => {
   const series = await Serie.find({})
   res.send(series)
 })
-router.get('/:id', (req, res) => {
+router.post('/', async (req, res) => {
+ 
+ const serie = new Serie (req.body)
+  try{
+    await serie.save()
+    res.send(serie)
+  }catch(e){
+    res.send({
+      success: false,
+      errors: Object.keys(e.errors)
+    })
+  }
+})
+/*router.delete('/:id', async (req, res) => {
+  await Serie.remove({ _id: req.params.id})
+  res.send({
+    success: true
+  })
+})*/
+router.get('/:id', async (req, res) => {
   res.send(req.params.id)
 })
 
